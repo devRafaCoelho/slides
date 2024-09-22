9f76e825ca8d943ec14c4d926ece536ae3e012dc
Rafael Coelho <devrafaelcoelho@gmail.com>
2024-09-22 11:39:44 -0300

    created calculate slide distance

--- js/slide.js
+++ js/slide.js
@@ -2,25 +2,41 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.distance = {
+      finalPosition: 0,
+      startX: 0,
+      movement: 0,
+    };
 
     this.handleStart = this.handleStart.bind(this);
     this.handleEndEvent = this.handleEndEvent.bind(this);
     this.handleMove = this.handleMove.bind(this);
   }
 
+  handleMoveSlide(distanceX) {
+    this.distance.movePosition = distanceX;
+    this.slide.style.transform = `translate3d(${distanceX}px, 0, 0)`;
+  }
+
+  handleUpdatePosition(clientX) {
+    this.distance.movement = this.distance.startX - clientX;
+    return this.distance.finalPosition - this.distance.movement;
+  }
+
   handleStart(event) {
     event.preventDefault();
-    console.log("started");
-    this.wrapper.addEventListener("mousedown", this.handleMove);
+    this.distance.startX = event.clientX;
+    this.wrapper.addEventListener("mousemove", this.handleMove);
   }
 
   handleMove(event) {
-    console.log("moved");
+    const updatedPosition = this.handleUpdatePosition(event.clientX);
+    this.handleMoveSlide(updatedPosition);
   }
 
   handleEndEvent() {
-    console.log("finished");
-    this.wrapper.removeEventListener("mousedown", this.handleMove);
+    this.wrapper.removeEventListener("mousemove", this.handleMove);
+    this.distance.finalPosition = this.distance.movePosition;
   }
 
   addEvents() {
