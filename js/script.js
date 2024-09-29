56f1a8fdd551bc60b7b6d834bc552b0815e0bff4
Italo Nascimento <iferreira@brivia.com.br>
2024-09-29 10:51:07 -0300

    created custom control

--- js/script.js
+++ js/script.js
@@ -3,5 +3,4 @@ import { SlideNav } from "./slide.js";
 const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
 slide.handleAddArrow(".prev", ".next");
-
-slide.handleAddEventControl();
+slide.handleAddEventControl(".custom-control");
